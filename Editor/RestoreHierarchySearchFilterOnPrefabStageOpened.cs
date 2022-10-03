7830b58c032f3977b5b45046e973350d97305c41
baba-s <shotababa1206@gmail.com>
2022-10-03 19:32:01 +0900

    add

---
+++ Editor/RestoreHierarchySearchFilterOnPrefabStageOpened.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Kogane.Internal
+{
+    [InitializeOnLoad]
+    internal static class RestoreHierarchySearchFilterOnPrefabStageOpened
+    {
+        static RestoreHierarchySearchFilterOnPrefabStageOpened()
+        {
+            // 現在開いているプレハブを開くときは
+            // prefabStageOpened が呼び出されないため
+            // 検索欄の文字列は復元できない
+            PrefabStage.prefabStageOpened -= Restore;
+            PrefabStage.prefabStageOpened += Restore;
+        }
+
+        private static void Restore( PrefabStage prefabStage )
+        {
+            var searchFilter = SceneHierarchyInternal.SearchFilter;
+
+            EditorApplication.delayCall += () => SceneHierarchyInternal.SetSearchFilter
+            (
+                searchFilter: searchFilter,
+                searchMode: SearchableEditorWindow.SearchMode.All,
+                setAll: true
+            );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.RestoreHierarchySearchFilterOnPrefabStageOpened
+# Kogane Restore Hierarchy Search Filter On Prefab Stage Opened
+
+プレハブモードを開く時に Hierarchy の検索欄を保持するエディタ拡張
